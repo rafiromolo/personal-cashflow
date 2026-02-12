767b7cefe9484f0d1459d7c9a0c2754a31597b0d
rafiromolo <rafiromolo@gmail.com>
2026-02-12 22:35:40 +0700

    add: card component

---
+++ src/components/display/Cards.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+    <div class="rounded-2xl border border-gray-200 bg-white p-5 md:p-6">
+      <div class="flex flex-row items-center gap-4">
+        <div class="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <g fill="none" fill-rule="evenodd">
+              <path
+                d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"
+              />
+              <path
+                fill="currentColor"
+                d="M5 4a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zm15 4.17V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v1.17c.313-.11.65-.17 1-.17h14c.35 0 .687.06 1 .17M4 11v1h5q.042 0 .084.004a1 1 0 0 1 1.03.663a2.001 2.001 0 0 0 3.772 0a1 1 0 0 1 1.03-.663q.041-.005.084-.004h5v-1a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1m11.465 3A4 4 0 0 1 12 16a4 4 0 0 1-3.465-2H4v3a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-3z"
+              />
+            </g>
+          </svg>
+        </div>
+        <span class="text-md font-bold uppercase">Income</span>
+      </div>
+      <div class="mt-4">
+        <h1 class="text-4xl font-black">Rp5,654,174</h1>
+      </div>
+      <div class="mt-6 flex items-center gap-2">
+        <span
+          class="flex items-center gap-1 w-fit rounded-sm py-0.5 px-2 bg-green-50 text-sm font-semibold text-green-600 border border-green-700"
+        >
+          <svg
+            class="fill-current"
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M5.56462 1.62393C5.70193 1.47072 5.90135 1.37432 6.12329 1.37432C6.1236 1.37432 6.12391 1.37432 6.12422 1.37432C6.31631 1.37415 6.50845 1.44731 6.65505 1.59381L9.65514 4.5918C9.94814 4.88459 9.94831 5.35947 9.65552 5.65246C9.36273 5.94546 8.88785 5.94562 8.59486 5.65283L6.87329 3.93247L6.87329 10.125C6.87329 10.5392 6.53751 10.875 6.12329 10.875C5.70908 10.875 5.37329 10.5392 5.37329 10.125L5.37329 3.93578L3.65516 5.65282C3.36218 5.94562 2.8873 5.94547 2.5945 5.65248C2.3017 5.35949 2.30185 4.88462 2.59484 4.59182L5.56462 1.62393Z"
+              fill=""
+            />
+          </svg>
+          20%
+        </span>
+        <span class="text-sm text-gray-400 italic">increasing from previous period.</span>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,17 +1,21 @@
 <template>
   <admin-layout>
-    <div>
-      <h1>Ini adalah halaman Dashboard.vue</h1>
+    <div class="grid grid-cols-12 gap-4 md:gap-6">
+      <div class="col-span-12 space-y-6 xl:col-span-7">
+        <cards />
+      </div>
     </div>
   </admin-layout>
 </template>
 
 <script>
 import AdminLayout from '@/components/layout/AdminLayout.vue'
+import Cards from '@/components/display/Cards.vue'
 
 export default {
   components: {
     AdminLayout,
+    Cards,
   },
   name: 'DashboardView',
 }
