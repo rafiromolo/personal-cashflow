96711aa0cca22634150856c9c7474ef1a80f5db6
rafiromolo <rafiromolo@gmail.com>
2026-03-07 16:00:38 +0700

    add account information (setup page)

--- src/views/setup/Setup.vue
+++ src/views/setup/Setup.vue
@@ -7,6 +7,33 @@
         Cashflow.
       </span>
     </div>
+
+    <div class="w-full bg-white border border-gray-200 rounded-lg p-6 shadow-sm">
+      <h2 class="text-xl font-bold mb-2">Account Information</h2>
+
+      <form action="#">
+        <div class="grid grid-cols-2 gap-4">
+          <div class="flex flex-col mb-4">
+            <label for="account-name" class="text-sm font-normal mb-1">User Name</label>
+            <input
+              type="text"
+              id="account-name"
+              class="bg-gray-100 text-shadow-gray-900 py-1 px-4 rounded-sm border border-gray-300"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div class="flex flex-col mb-4">
+            <label for="account-email" class="text-sm font-normal mb-1">Email</label>
+            <input
+              type="email"
+              id="account-email"
+              class="bg-gray-100 text-shadow-gray-900 py-1 px-4 rounded-sm border border-gray-300"
+              placeholder="Enter your email"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   </admin-layout>
 </template>
 
